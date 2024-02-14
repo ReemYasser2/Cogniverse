6616bca15eb7cf7f6819c20840d5e4d84cf09410
Reem Yasser <reemyasser332@yahoo.com>
2024-02-14 19:21:32 +0200

    Game Planets Created

--- Assets/Scripts/SceneHandler.cs
+++ Assets/Scripts/SceneHandler.cs
@@ -5,20 +5,45 @@ using UnityEngine.SceneManagement;
 
 public class SceneHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // A script that handles moving between scenes to open games from the home screen
+    // & exit games returning to the home screen
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    // function that opens the dual n-back game
     public void OpenDualNback()
     {
         SceneManager.LoadScene(1);
     }
+
+    // function that opens the electrical maze game
+    public void OpenElectricalMaze()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    // function that opens the shuffled game
+    public void OpenShuffled()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    // function that opens the trail making game
+    public void OpenTrailMaking()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    // function that opens the focus fusion game
+    public void OpenFocusFusion()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    // function that opens the whack-a-mole game
+    public void OpenWhackAmole()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+
+
 }
