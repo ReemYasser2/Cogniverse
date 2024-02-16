04c8ca5d39c6dcfe7cb77cf0546da9ae90e956d4
RoaaEhab <73796828+RoaaEhab@users.noreply.github.com>
2024-02-16 22:58:26 +0200

    Update SpawnManager.cs

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,49 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject objectPrefab;
+    public GameObject endGamePanel;
+    public GameObject menuePanel;
     public Transform[] spawnPositions;
-    public Material[] materials;
     public Vector3 currentPosition;
     private Vector3 oldPosition;
     public float intervalBetweenSpawns = 2.0f;
     public bool isAPressed = false;
+    public TextMeshProUGUI scoreText;
+    public Button startButton;
     // Start is called before the first frame update
-    private void Start()
-    {
-        StartCoroutine(SpawnObjectsRandomly());
-    }
-
+    //private void Start()
+    //{
+        //StartCoroutine(SpawnObjectsRandomly());
+    //}
 
     IEnumerator SpawnObjectsRandomly()
     {
         while (AudioSpawnSharedVariables.trialsCount < AudioSpawnSharedVariables.maxTrials)
         {
             yield return new WaitForSeconds(intervalBetweenSpawns);
-
+            ScoreCalculator.reinforcementText = "";
             // Randomly selecting one of the positions
             int randomIndex = Random.Range(0, spawnPositions.Length);
             Vector3 spawnPos = spawnPositions[randomIndex].position;
 
-           
-
             // Instantiate the cube at the chosen position
-            GameObject newObject = Instantiate(objectPrefab, spawnPos, Quaternion.identity);// Get or add a Renderer component
-
-            // Randomly select a material from the materials array
-            int randomMaterialIndex = Random.Range(0, materials.Length);
-            Material randomMaterial = materials[randomMaterialIndex];
-
-            // Apply the material to the child object
-            Renderer childRenderer = newObject.transform.GetChild(0).GetComponent<Renderer>();
-            if (childRenderer != null)
-            {
-                childRenderer.material = randomMaterial;
-            }
-
+            GameObject newObject = Instantiate(objectPrefab, spawnPos, Quaternion.identity);
             ScoreCalculator.isCalculated = false;
             SetCurrentPosition(newObject);
 
@@ -62,6 +53,7 @@ public class SpawnManager : MonoBehaviour
             SetOldPosition(spawnPos);
             AudioSpawnSharedVariables.trialsCount++;
         }
+        ShowEndGamePanel();
     }
     void SetCurrentPosition(GameObject gameObject)
     {
@@ -87,4 +79,15 @@ public class SpawnManager : MonoBehaviour
         }
     }
 
+    private void ShowEndGamePanel()
+    {
+        endGamePanel.SetActive(true);
+        menuePanel.SetActive(false);
+        scoreText.text = $"Score: {ScoreCalculator.score}";
+    }
+
+    public void StartButtonClick()
+    {
+        StartCoroutine(SpawnObjectsRandomly());
+    }
 }
