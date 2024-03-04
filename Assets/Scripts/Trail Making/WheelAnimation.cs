6bbb90d5f3bed4f061b021ef0d880cf07ae7815c
RoaaEhab <73796828+RoaaEhab@users.noreply.github.com>
2024-03-04 22:07:36 +0200

    cabin swinging

--- Assets/Scripts/Trail Making/WheelAnimation.cs
+++ Assets/Scripts/Trail Making/WheelAnimation.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class WheelAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        transform.Rotate(0, 0, 50 * Time.deltaTime); //rotates 50 degrees per second around z axis
-    }
-
     // Update is called once per frame
     void Update()
     {
