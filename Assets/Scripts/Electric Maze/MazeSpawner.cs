86afe55cf5b80af889925cd5d4a534d8f85bfd80
FarahGamal <farah.ali02@eng-st.cu.edu.eg>
2024-03-06 19:53:08 +0200

    arrow for maze 2

--- Assets/Scripts/Electric Maze/MazeSpawner.cs
+++ Assets/Scripts/Electric Maze/MazeSpawner.cs
@@ -7,6 +7,8 @@ public class MazeSpawner : MonoBehaviour
     public GameObject[] mazePrefabs;
     public Transform spawnPosition;
     public int mazeSelection = 0;
+    public GameObject maze1Canvas;
+    public GameObject maze2Canvas;
 
     // This function chooses one maze randomly 
     public void SelectMazeRandomly()
@@ -16,10 +18,12 @@ public class MazeSpawner : MonoBehaviour
         if (mazeIndex == 0)
         {
             mazeSelection = 1;
+            maze1Canvas.SetActive(true);
         }
         else if (mazeIndex == 1)
         {
             mazeSelection = 2;
+            maze2Canvas.SetActive(true);
         }
     }
 }
