728233481ac79bc671aa3b02ea018319eb0abd1c
Hoda Azzam <hoda.eliwa00@eng-st.cu.edu.eg>
2024-03-06 15:21:54 +0200

    3 levels scoring done

--- Assets/Scripts/Trail Making/LevelsHandler.cs
+++ Assets/Scripts/Trail Making/LevelsHandler.cs
@@ -9,8 +9,12 @@ public class LevelsHandler : MonoBehaviour
     public bool level_2= false;
     public bool level_3= false;
 
-    public GameObject[] trails; 
-    
+
+    public int selectedTrail = 0;
+
+    public GameObject[] levelOneTrails = new GameObject[2];
+    public GameObject[] levelTwoTrails = new GameObject[2];
+    public GameObject[] levelThreeTrails = new GameObject[2];
 
 
     // Start is called before the first frame update
@@ -18,18 +22,22 @@ public class LevelsHandler : MonoBehaviour
     {
         if (level_1)
         {
-            int trailIndex = Random.Range(0, 1);
-            trails[trailIndex].SetActive(true);
+            int trailIndex = Random.Range(0, levelOneTrails.Length);
+            Debug.Log(levelOneTrails[trailIndex]); // NULL????!!!!
+            levelOneTrails[trailIndex].SetActive(true);
+            selectedTrail = trailIndex;
         }
         if (level_2)
         {
-            int trailIndex = Random.Range(2, 3);
-            trails[trailIndex].SetActive(true);
+            int trailIndex = Random.Range(0, levelTwoTrails.Length);
+            levelTwoTrails[trailIndex].SetActive(true);
+            levelOneTrails[trailIndex].SetActive(false); ;
         }
         if (level_3)
         {
-            int trailIndex = Random.Range(4, 5);
-            trails[trailIndex].SetActive(true);
+            int trailIndex = Random.Range(0, levelThreeTrails.Length);
+            levelThreeTrails[trailIndex].SetActive(true);
+            levelTwoTrails[trailIndex].SetActive(false);
         }
     }
     
