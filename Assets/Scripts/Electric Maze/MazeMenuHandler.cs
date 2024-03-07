0a385c51e380035a31e184abe1f5cf0934f835a1
FarahGamal <farah.ali02@eng-st.cu.edu.eg>
2024-03-07 23:49:08 +0200

    edit instructions + menu for trail & maze

---
+++ Assets/Scripts/Electric Maze/MazeMenuHandler.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeMenuHandler : MonoBehaviour
+{
+    /*
+    public GameObject instructionsLevel1Canvas;
+    public GameObject instructionsLevel2Canvas;
+    public GameObject menuCanvas;
+
+    public void MazeInstructionsHandler()
+    {
+        if (Level1)
+        {
+            instructionsLevel1Canvas.SetActive(true);
+        }
+        else if (Level2)
+        {
+            instructionsLevel2Canvas.SetActive(true);
+        }
+    }
+
+    public void ShowMenuHandlerMaze()
+    {
+        if (GameStart && !GameOver)
+        {
+            if (menuCanvas.activeSelf == false)
+            {
+                menuCanvas.SetActive(true);
+                PauseGame.Pause();
+            }
+        }
+    }
+    */
+}
---
+++ Assets/Scripts/Trail Making/TrailMenuHandler.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailMenuHandler : MonoBehaviour
+{
+    /*
+    public GameObject instructionsLevel1Canvas;
+    public GameObject instructionsLevel2Canvas;
+    public GameObject instructionsLevel3Canvas;
+    public GameObject menuCanvas;
+
+    public void MazeInstructionsHandler()
+    {
+        if (Level1)
+        {
+            instructionsLevel1Canvas.SetActive(true);
+        }
+        else if (Level2)
+        {
+            instructionsLevel2Canvas.SetActive(true);
+        }
+        else if (Level3)
+        {
+            instructionsLevel3Canvas.SetActive(true);
+        }
+    }
+
+    public void ShowMenuHandlerMaze()
+    {
+        if (GameStart && !GameOver)
+        {
+            if (menuCanvas.activeSelf == false)
+            {
+                menuCanvas.SetActive(true);
+                PauseGame.Pause();
+            }
+        }
+    }
+    */
+}
