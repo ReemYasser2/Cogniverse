f0f1b3730677835a7d9f1eeda6224f6f08699ae5
FarahGamal <farah.ali02@eng-st.cu.edu.eg>
2024-03-13 21:11:01 +0200

    whack a mole UI + Timer

---
+++ Assets/Scripts/Whack A Mole/WhackTimer.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WhackTimer : MonoBehaviour
+{
+    [SerializeField] TMP_Text timerText;
+    public float remainingTime;
+    private bool isPlayPressed = false;
+    public static bool isTimeOver = false;
+    public GameObject gameOverCanvas;
+
+    public Color criticalColor = Color.red;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // remainingTime = GeneralCountDownTimer.TimerInitialization(timerText, isLevel1, isLevel2, 5, 10);
+
+        timerText.color = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayPressed)
+        {
+            // GeneralCountDownTimer.TimerUpdate(timerText, ref isTimeOver, ref remainingTime);
+
+            if (remainingTime > 0)
+            {
+                remainingTime -= Time.deltaTime;
+
+                if (remainingTime <= 5)
+                {
+                    timerText.color = criticalColor;
+                }
+            }
+            else if (remainingTime <= 0)
+            {
+                remainingTime = 0;
+                // Timer is over
+                isTimeOver = true;
+                gameOverCanvas.SetActive(true);
+            }
+            int minutes = Mathf.FloorToInt(remainingTime / 60);
+            int seconds = Mathf.FloorToInt(remainingTime % 60);
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+    }
+
+    // This function is called when the game starts to start the timer
+    public void StartGame()
+    {
+        isPlayPressed = true;
+        isTimeOver = false;
+        timerText.color = Color.white;
+
+        //remainingTime = GeneralCountDownTimer.TimerInitialization(timerText, isLevel1, isLevel2, 5, 10);
+    }
+}
