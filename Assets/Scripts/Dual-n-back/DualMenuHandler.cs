5ec5def22207c35e8904cf7a2a8de8433b6fbad0
FarahGamal <farah.ali02@eng-st.cu.edu.eg>
2024-03-14 21:03:11 +0200

    dual n back: fix menu + change play icon

--- Assets/Scripts/Dual-n-back/DualMenuHandler.cs
+++ Assets/Scripts/Dual-n-back/DualMenuHandler.cs
@@ -16,33 +16,10 @@ public class DualMenuHandler : MonoBehaviour
     public void InstructionsHandler()
     {
         GeneralMenuHandler.InstructionsHandler(ScoreCalculator.isLevel1, ScoreCalculator.isLevel2, ScoreCalculator.isLevel3, instructionsLevel1MenuCanvas, instructionsLevel2MenuCanvas, instructionsLevel3MenuCanvas);
-        /*
-        if (ScoreCalculator.isLevel1)
-        {
-            instructionsLevel1Canvas.SetActive(true);
-        }
-        else if (ScoreCalculator.isLevel2)
-        {
-            instructionsLevel2Canvas.SetActive(true);
-        }
-        else if(ScoreCalculator.isLevel3)
-        {
-            instructionsLevel3Canvas.SetActive(true);
-        }
-        */
     }
 
     public void ShowMenuHandler()
     {
-        //GeneralMenuHandler.ShowMenuHandler(ScoreCalculator.isGameStart, ScoreCalculator.isLevel2, ScoreCalculator.isLevel3, ScoreCalculator.isGameOver, menuCanvas, instructionsLevel1Canvas, instructionsLevel2Canvas, instructionsLevel3Canvas, instructionsLevel1MenuCanvas, instructionsLevel2MenuCanvas, instructionsLevel3MenuCanvas, 1, 3);
-
-        if (ScoreCalculator.isGameStart && !ScoreCalculator.isGameOver)
-        {
-            if (menuCanvas.activeSelf == false)
-            {
-                menuCanvas.SetActive(true);
-                PauseGame.Pause();
-            }
-        }
+        GeneralMenuHandler.ShowMenuHandler(ScoreCalculator.isGameStart, ScoreCalculator.isLevel2, ScoreCalculator.isLevel3, ScoreCalculator.isGameOver, menuCanvas, instructionsLevel1Canvas, instructionsLevel2Canvas, instructionsLevel3Canvas, instructionsLevel1MenuCanvas, instructionsLevel2MenuCanvas, instructionsLevel3MenuCanvas, 1, 3);
     }
 }
