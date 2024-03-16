742ca2ffbe62c1eeee0b9ba26789dad9b7214da5
Hoda Azzam <hoda.eliwa00@eng-st.cu.edu.eg>
2024-03-16 02:38:03 +0200

    Focus Fusion Spawning Updated

--- Assets/Scripts/Focus Fusion/GridSpawner.cs
+++ Assets/Scripts/Focus Fusion/GridSpawner.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using Unity.XR.Oculus;
+using UnityEditor.Rendering;
 
 public class GridSpawner : MonoBehaviour
 {
     public GameObject[] gridsPrefabs;
     public Transform spawnPosition; 
     public Transform[] levelTwoSpawnPos;
+    GameObject newObject;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,13 +26,18 @@ public class GridSpawner : MonoBehaviour
     {
         if (level == 1)
         {
-            while (!FocusTimer.isTimeOver)
+          //  while (!FocusTimer.isTimeOver)
             {
+                
                 int randomIndex = Random.Range(0, gridsPrefabs.Length);
 
-                GameObject newObject = Instantiate(gridsPrefabs[randomIndex], spawnPosition.position, Quaternion.identity);
-                yield return new WaitForSeconds(0.8f);
-                Destroy(newObject);
+                newObject = Instantiate(gridsPrefabs[randomIndex], spawnPosition.position, Quaternion.identity);
+                yield return new WaitForSeconds(0.75f);
+                if (newObject != null)
+                {
+                    newObject.SetActive(false);
+                   // Destroy(newObject);
+                }
             }
         }
         else if (level == 2)
@@ -37,15 +46,33 @@ public class GridSpawner : MonoBehaviour
             {
                 int randomIndex = Random.Range(0, gridsPrefabs.Length);
                 int randomPosIndex = Random.Range(0, levelTwoSpawnPos.Length);
-                GameObject newObject = Instantiate(gridsPrefabs[randomIndex], levelTwoSpawnPos[randomPosIndex].position, Quaternion.identity);
-                yield return new WaitForSeconds(0.8f);
-                Destroy(newObject);
+                newObject = Instantiate(gridsPrefabs[randomIndex], levelTwoSpawnPos[randomPosIndex].position, Quaternion.identity);
+                yield return new WaitForSeconds(1);
+                if (newObject != null)
+                {
+                    newObject.SetActive(false);
+                    Destroy(newObject);
+                }
             }
         }
-    } 
+    }
     public void StartSpawning(int level)
     {
         StartCoroutine(RandomSpawner(level));
+    }
+    public void GetUserResponse()
+    {
+        Destroy(newObject);
+        Debug.Log("AAAAAAAAAAAAA");
+    }
+    public void UserReleased()
+    {
+        int randomIndex = Random.Range(0, gridsPrefabs.Length);
+        newObject = Instantiate(gridsPrefabs[randomIndex], spawnPosition.position, Quaternion.identity); newObject.SetActive(true);
+        new WaitForSeconds(0.75f);
+        Debug.Log("Hii");
+        newObject.SetActive(false);
+        Destroy(newObject);
     }
 
 }
