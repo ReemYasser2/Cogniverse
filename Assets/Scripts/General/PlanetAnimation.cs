deed9ca13e41a816a383ae6fe12f7b4092a514a0
RoaaEhab <73796828+RoaaEhab@users.noreply.github.com>
2024-03-23 13:21:04 +0200

    Planets' animation

---
+++ Assets/Scripts/General/PlanetAnimation.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetAnimation : MonoBehaviour
+{
+    public float rotationAngle = 30.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotationAngle * Time.deltaTime, 0); 
+    }
+
+}
